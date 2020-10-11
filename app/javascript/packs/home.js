1def21186ed151f792fc98c914da06b4e6a1d837
BMuscle <kd1242488@st.kobedenshi.ac.jp>
2020-10-11 21:55:51 +0900

    vuex-router-syncの追加
    storeの修正も行った

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,19 +1,22 @@
 import Vue from "vue";
-import Router from "./router/router";
+import router from "./router/router";
 import App from "../app";
 import i18n from "./i18n/i18n";
 import store from "./store/store";
+import { sync } from "vuex-router-sync";
 
 Vue.mixin({
-  created: function () {
-    this.$store.dispatch("setCurrentUser");
+  created: function() {
+    store.dispatch("setCurrentUser");
   },
 });
+
+sync(store, router);
 
 window.addEventListener("DOMContentLoaded", () => {
   new Vue({
     store,
-    router: Router,
+    router: router,
     el: "#app",
     i18n,
     render: (h) => h(App),
