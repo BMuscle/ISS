e52e7f46dbe4a501e9615d27af2055448c63ecb3
BMuscle <kd1242488@st.kobedenshi.ac.jp>
2020-10-12 00:03:32 +0900

    カテゴリー登録のコントローラ部分追加
    エラーメッセージの日本語化。

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -3,7 +3,23 @@ module Api
     class CategoriesController < ApplicationController
       before_action :get_categories, only: :index
 
+      def create
+        p "-------------"
+        p category_params
+        p "-------------"
+        @category = Category.new(category_params)
+        if @category.save
+          head :no_content
+        else
+          render json: @category.errors.full_messages, status: :unprocessable_entity
+        end
+      end
+
       private
+
+      def category_params
+        params.require(:category).permit(:name)
+      end
 
       def get_categories
         @categories = Category.all
